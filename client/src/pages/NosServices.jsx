89640fa2c156cc89ba689704604899540ee32c1c
DaliaBelharet <daliabelharett@gmail.com>
2024-06-19 21:23:49 +0200

    19-06

--- client/src/pages/NosServices.jsx
+++ client/src/pages/NosServices.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import Navbar from '../components/Navbar1';
+import Navbar from '../components/Navbar';
+import Navbar1 from '../components/Navbar1';
 import Footer from '../components/footer';
 import Propertie from '../components/propertie';
+import { useSelector } from 'react-redux';
 
 
 const NosServices = () => {
+
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <div>
       <div className="navbar"> 
-        <Navbar />
+      {currentUser ? <Navbar1 /> : <Navbar />}
       </div>
 
       <div className="propertie"> 
