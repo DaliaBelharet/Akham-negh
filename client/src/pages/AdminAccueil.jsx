00191f121911836e72c86a7ea158d263d994c1f3
DaliaBelharet <daliabelharett@gmail.com>
2024-06-19 21:31:08 +0200

    19-06

--- client/src/pages/AdminAccueil.jsx
+++ client/src/pages/AdminAccueil.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import Sidebar from '../components/SideBar'; 
 import panel from '../assets/Control Panel-cuate.png';
 import { useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 function AdminAccueil({admin}) {
     const { currentAdmin } = useSelector((state) => state.admin);
 
     console.log('currentAdmin :', currentAdmin);
     console.log('Admin :', admin);
+
     const styles = {
         adminAccueilContainer: {
             display: 'flex',
             height: '100vh',
+            position: 'relative', 
         },
         sidebar: {
             width: '200px',
@@ -42,6 +47,20 @@ function AdminAccueil({admin}) {
             fontSize: '24px',
             fontWeight: 'bold',
         },
+        homeLink: {
+            position: 'absolute',
+            bottom: '20px',
+            right: '20px',
+            display: 'flex',
+            alignItems: 'center',
+            textDecoration: 'none',
+            color: 'black',
+            fontSize: '18px',
+            fontWeight: 'bold',
+        },
+        icon: {
+            marginRight: '8px',
+        }
     };
 
     return (
@@ -52,9 +71,13 @@ function AdminAccueil({admin}) {
             <div style={styles.mainContent}>
                 <div style={styles.headerSection}>
                     <h1 style={styles.title}>Effectuez des actions administratives en toute simplicité dans votre Dashboard.</h1>
-                    <img src={panel} alt="Description of the image" style={styles.headerImage} />
+                    <img src={panel} alt="" style={styles.headerImage} />
                 </div>
             </div>
+            <Link to="/" style={styles.homeLink}>
+                <FontAwesomeIcon icon={faArrowLeft} style={styles.icon} />
+                Retour à la page d'accueil
+            </Link>
         </div>
     );
 }
